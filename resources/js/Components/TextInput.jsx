bf37f33aa0032bdd3f600c2771926fc306b3fd2f
kmpzin <kempfervictor@gmail.com>
2023-09-23 18:11:21 -0300

    feat: change aspas

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,9 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from "react";
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput(
+    { type = "text", className = "", isFocused = false, ...props },
+    ref
+) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -14,7 +17,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
             {...props}
             type={type}
             className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+                "border-gray-300 dark:text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" +
                 className
             }
             ref={input}
