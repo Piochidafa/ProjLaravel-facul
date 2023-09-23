7a1edf116a39758bf9403e3b91720fd2361bb5f0
kmpzin <kempfervictor@gmail.com>
2023-09-23 18:11:33 -0300

    feat: create route

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/produto', function () {
+    return Inertia::render('Produto');
+})->middleware(['auth', 'verified'])->name('produto');
+
 
 
 Route::middleware('auth')->group(function () {
