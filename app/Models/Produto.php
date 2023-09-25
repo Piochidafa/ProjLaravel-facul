bb6a0bc8a7abb52631b402e37a72078fc6d33d99
kmpzin <kempfervictor@gmail.com>
2023-09-25 02:48:41 -0300

    feat: add data to model

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Produto extends Model
+    class Produto extends Model
 {
     use HasFactory;
+
+    protected $table = "produto";
+    protected $fillable = [
+        'produto_nome',
+        'produto_preco',
+        'produto_descricao',
+        'produto_peso',
+        'produto_tamanho',
+        'produto_material',
+        'produto_categoria',
+        'produto_fornecedor',
+    ];
 }
