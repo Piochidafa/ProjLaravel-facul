db578de16e8a4578d46c03ecb6240a91cd507921
lyssautida <lyssamailbox@gmail.com>
2023-10-15 19:09:24 -0400

    feat: produtos migrate, model

---
+++ app/Models/Produto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produtos';
+    protected fillable = [
+        'id';
+        'nome';
+        'categoria';
+        'peso';
+        'material';
+        'tamanho';
+        'variacao';
+        'unidade';
+        ]
+}
