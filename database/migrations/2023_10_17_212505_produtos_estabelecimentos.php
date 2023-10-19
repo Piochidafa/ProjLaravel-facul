791d181aec993e566ca14c56b6c47da749e10257
AdrielBegin <adriel.pemi@gmail.com>
2023-10-19 17:13:26 -0400

    feat: criando a migrations de fornecedor e produtos_fornecedor

--- database/migrations/2023_10_17_212505_produtos_estabelecimentos.php
+++ database/migrations/2023_10_17_212505_produtos_estabelecimentos.php
@@ -4,14 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::create("produtos_estabelecimento", function (Blueprint $table) {
+            $table->id("id");
+            $table->unsignedBigInteger("produto_id");
+            $table->unsignedBigInteger("estabelecimento_id");
+            $table->unsignedBigInteger("fornecedor_id");
+            $table->string("preco");
+            $table->string("descricao");
+            $table->dateTime("updated_at");
+            $table->dateTime("created_at");
+            $table->dateTime('inactivated_at')->nullable();
+            $table->foreign("produto_id")->references("id")->on("produtos");
+            $table->foreign("estabelecimento_id")->references("id")->on("estabelecimento");
+            $table->foreign("fornecedor_id")->references("id")->on("fornecedor");
+        });
     }
 
     /**
