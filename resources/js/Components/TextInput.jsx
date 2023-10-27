10e105791ef22c30e1453f3a612815b9bb740add
Alan <129499674+algehring@users.noreply.github.com>
2023-10-26 20:17:13 -0400

    feat: change text color

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -14,7 +14,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
             {...props}
             type={type}
             className={
-                'border-gray-300 dark:text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+                'border-gray-300 text-500 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
                 className
             }
             ref={input}
