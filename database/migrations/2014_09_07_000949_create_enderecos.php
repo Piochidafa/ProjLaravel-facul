04cb6c81175dfd6522e09e01444e4a685906b62a
lyssadeveloper <lyssamailbox@gmail.com>
2023-11-13 19:58:17 -0400

    refactor: delete created_at and updated_at fields in address table

--- database/migrations/2014_09_07_000949_create_enderecos.php
+++ database/migrations/2014_09_07_000949_create_enderecos.php
@@ -16,8 +16,6 @@ return new class extends Migration {
             $table->string('cep');
             $table->string('cidade');
             $table->string('estado');
-            $table->dateTime('updated_at');
-            $table->dateTime('created_at');
         });
     }
 
