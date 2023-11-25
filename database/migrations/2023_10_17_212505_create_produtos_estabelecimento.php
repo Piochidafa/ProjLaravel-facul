5474167aa02795a4b806a72dcabb68a56eac9dbd
Victor Kempfer <kempfervictor@gmail.com>
2023-11-25 13:46:24 -0300

    feat: commented line 20 to line 25

--- database/migrations/2023_10_17_212505_create_produtos_estabelecimento.php
+++ database/migrations/2023_10_17_212505_create_produtos_estabelecimento.php
@@ -11,18 +11,18 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create("produtos_estabelecimento", function (Blueprint $table) {
-            $table->bigIncrements("id");          
+            $table->bigIncrements("id");
             $table->string("preco")->default(0);
             $table->string("descricao");
             $table->dateTime("updated_at");
             $table->dateTime("created_at");
             $table->dateTime('inactivated_at')->nullable();
-            $table->unsignedBigInteger('produto_id');
-            $table->unsignedBigInteger('estabelecimento_id');
-            $table->unsignedBigInteger('fornecedor_id');
-            $table->foreign("produto_id")->references("id")->on("produtos");
-            $table->foreign("estabelecimento_id")->references("id")->on("estabelecimentos");
-            $table->foreign("fornecedor_id")->references("id")->on("fornecedores");
+            // $table->unsignedBigInteger('produto_id');
+            // $table->unsignedBigInteger('estabelecimento_id')->nullable();
+            // $table->unsignedBigInteger('fornecedor_id');
+            // $table->foreign("produto_id")->references("id")->on("produtos");
+            // $table->foreign("estabelecimento_id")->references("id")->on("estabelecimentos");
+            // $table->foreign("fornecedor_id")->references("id")->on("fornecedores");
         });
     }
 
